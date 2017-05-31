c0e2ec6b9f3194b40f0b3c95c6d9aaf67ade2bcf
Julia <jmishkovets@gmail.com>
2017-05-31 11:30:52 +0200

    Integrate MaterialCollections

--- MDC-Tutorial/ViewController.swift
+++ MDC-Tutorial/ViewController.swift
@@ -7,19 +7,37 @@
 //
 
 import UIKit
+import MaterialComponents.MaterialCollections
 
-class ViewController: UIViewController {
+class ViewController: MDCCollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        styler.cellStyle = .card
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: UICollectionViewDataSource
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 5
     }
-
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        // Add some mock text to the cell.
+        if let textCell = cell as? MDCCollectionViewTextCell {
+            let animals = ["Lions", "Tigers", "Bears", "Monkeys"]
+            textCell.textLabel?.text = animals[indexPath.item]
+        }
+        
+        return cell
+    }
 
 }
 
